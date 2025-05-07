5e21cb04c199b67757954dabad756c846d9aa156
yuweiyang-anu <yuwei.yang@anu.edu.au>
2025-05-07 18:47:45 +0800

    Delete scripts/tokens/extract_token_for_test.sh

--- scripts/tokens/extract_token_for_test.sh
+++
@@ -1,9 +0,0 @@
-torchrun \
- --nnodes=1 --nproc_per_node=4 --master_port 2328 \
- /openseg_blob/v-yuweiyang/SimpleAR/simpar/data/extract_token.py \
-    --dataset_type "image" \
-    --dataset_name "code2chart_debug_only" \
-    --code_path /openseg_blob/v-yuweiyang/SimpleAR/datasets/visual_tokens \
-    --gen_data_path /openseg_blob/v-yuweiyang/SimpleAR/datasets/code2chart_converted_10k_debug.json \
-    --gen_image_folder "" \
-    --gen_resolution 512
